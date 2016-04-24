9ba5b1b04315b640ef950d65da7807d75dfdd415
kyanro <gw.fnct+github_pub@gmail.com>
2016-04-24 22:57:12 +0900

    ファイルの読み込み完了
    リファクタリングの時に node_moduleの中身まで変わってしまわないようにnamespaceを利用して開発するようにした

---
+++ work/3/main.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * Created by ppp on 2016/04/24.
+ */
+var fs = require("fs");
+var main;
+(function (main) {
+    "use strict";
+    var argv = process.argv[2];
+    var buf = fs.readFileSync(argv);
+    var sum = buf.toString().split("")
+        .filter(function (char) { return char === "\n"; })
+        .map(function (char) { return 1; })
+        .reduce(function (prev, current) { return prev + current; });
+    console.log(sum);
+})(main || (main = {}));
--- work/3/main.ts
+++ work/3/main.ts
@@ -1,3 +1,15 @@
 /**
  * Created by ppp on 2016/04/24.
  */
+import * as fs from "fs";
+namespace main {
+    "use strict";
+    let argv: string = process.argv[2];
+    let buf: Buffer = fs.readFileSync(argv);
+
+    let sum: number = buf.toString().split("")
+        .filter((char: string) => char === "\n")
+        .map((char: string) => 1)
+        .reduce((prev: number, current: number) => prev + current);
+    console.log(sum);
+}
