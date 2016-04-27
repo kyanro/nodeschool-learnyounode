fe0cdd82c3438520f15ecbb8a0ef7de260d75b6d
kyanro <gw.fnct+github_pub@gmail.com>
2016-04-28 03:59:01 +0900

    8完了

---
+++ work/8/main.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * Created by ppp on 2016/04/28.
+ */
+var http = require("http");
+var main;
+(function (main) {
+    var url = process.argv[2];
+    http.get(url, function (response) {
+        var body = "";
+        response.setEncoding("utf8");
+        response.on("data", function (listener) {
+            body += listener;
+        });
+        response.on("end", function () {
+            console.log(body.length);
+            console.log(body);
+        });
+    });
+})(main || (main = {}));
---
+++ work/8/main.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by ppp on 2016/04/28.
+ */
+import * as http from "http";
+import {IncomingMessage} from "http";
+
+namespace main {
+    let url: string = process.argv[2];
+    http.get(url, (response: IncomingMessage) => {
+        let body: string = "";
+        response.setEncoding("utf8");
+        response.on("data", (listener: string) => {
+            body += listener;
+        });
+        response.on("end", () => {
+            console.log(body.length);
+            console.log(body);
+        });
+    });
+}
